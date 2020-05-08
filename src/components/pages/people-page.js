7bc86b960ec915faa6318e4802c64b58e90a5b44
kirilldd <36417808+kirilldd@users.noreply.github.com>
2020-05-08 21:15:02 +0300

    commit

---
+++ src/components/pages/people-page.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import { PersonList,PersonDetails } from '../sw-components';
+import { withRouter } from 'react-router-dom';
+import Row from '../row'
+
+const PeoplePage = ({history,match}) => {
+
+  const {id} = match.params;
+
+    return (
+      <Row
+        left={<PersonList onItemSelected={ (id) => history.push(id)} />}
+        right={<PersonDetails itemId={id} />} />
+    );
+  
+
+}
+
+export default withRouter(PeoplePage);
